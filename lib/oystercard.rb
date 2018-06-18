8309129593c5f15a38b2f89e73f30efd6f45c16c
Yvonne Tang <ytang604@gmail.com>
2018-06-18 16:29:41 +0100

    add balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,10 @@
 class Oystercard
-end 
+
+attr_reader :balance
+DEFAULT_BALANCE = 0 
+
+def initialize
+  @balance = DEFAULT_BALANCE
+end
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,9 @@ require 'oystercard'
 
 describe Oystercard do
 
+it 'should have a balance' do
+  oystercard = Oystercard.new
+  expect(subject.balance).to eq 0
+
+end
 end
