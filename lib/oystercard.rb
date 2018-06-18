f588aa99468a7541728faa8c5654304c9957074d
Yvonne Tang <ytang604@gmail.com>
2018-06-18 17:28:20 +0100

    maximum_balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,14 @@
 class Oystercard
 
 attr_reader :balance
-
+MAXIMUM_BALANCE = 90
 
 def initialize
   @balance = 0
 end
 
 def top_up(money)
+   fail "Cannot exceed £90" if money + balance > MAXIMUM_BALANCE
     @balance += money
     return @balance
   end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,11 +7,20 @@ it 'should have a balance' do
   expect(subject.balance).to eq 0
 end
 
-describe '#top_up' do 
+describe '#top_up' do
  it 'can be topped up' do
    oystercard = Oystercard.new
    subject.top_up(10)
    expect(subject.balance).to eq 10
    #expect subject=oystercard's balance to equal to 10
  end
+end
+
+ describe "maximum_balance" do
+   it 'cannot exceed (90)' do
+   maximum_balance = Oystercard::MAXIMUM_BALANCE
+   subject.top_up(maximum_balance)
+   expect {subject.top_up(1)}.to raise_error "Cannot exceed £90"
+  end
+  end
 end
